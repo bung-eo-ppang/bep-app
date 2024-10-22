20f5d5edf96916393cf9d2eb00d7471aa3dc8b82
2paperstar <lee@paperst.ar>
2024-10-22 12:46:03 +0900

    chore: apply `vite-plugin-electron-renderer`

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,20 +1,21 @@
-import { resolve } from 'path'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
+import react from '@vitejs/plugin-react';
+import renderer from 'vite-plugin-electron-renderer';
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
   },
   preload: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
   },
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
+        '@renderer': resolve('src/renderer/src'),
+      },
     },
-    plugins: [react()]
-  }
-})
+    plugins: [react(), renderer({ resolve: { serialport: { type: 'cjs' } } })],
+  },
+});
