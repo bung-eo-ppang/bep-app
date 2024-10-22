94700b4bf59e4a83aa407c370f13d69294f502f3
2paperstar <lee@paperst.ar>
2024-10-22 13:29:31 +0900

    chore: init tailwindcss

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,3 @@
-import Versions from './components/Versions';
-import electronLogo from './assets/electron.svg';
 import { SerialPort } from 'serialport';
 import { useEffect, useState } from 'react';
 
@@ -13,37 +11,8 @@ const usePorts = () => {
   return ports;
 };
 
-function App(): JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping');
+export const App = () => {
   const ports = usePorts();
 
-  return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      {JSON.stringify(ports)}
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
-  );
-}
-
-export default App;
+  return <>{JSON.stringify(ports)}</>;
+};
