66f8ce6c5b8146a8e9c33acc632ab2a490108344
2paperstar <lee@paperst.ar>
2024-10-22 13:39:03 +0900

    feat: port select list

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -14,5 +14,23 @@ const usePorts = () => {
 export const App = () => {
   const ports = usePorts();
 
-  return <>{JSON.stringify(ports)}</>;
+  return (
+    <div className="grid place-items-center min-h-screen">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-2xl font-bold">Select Board Port</h1>
+        <div className="flex gap-2">
+          <select name="" id="" className="border border-gray-300 rounded p-1">
+            {ports.map((port) => (
+              <option key={port.path} value={port.path}>
+                {port.path}
+              </option>
+            ))}
+          </select>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Connect
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
