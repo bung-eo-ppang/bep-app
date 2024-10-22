13a2413de2cbc7f196b786f12e0664b6a08628c5
2paperstar <lee@paperst.ar>
2024-10-22 14:08:39 +0900

    feat: add simple canvas

--- src/renderer/src/routes/screen.tsx
+++ src/renderer/src/routes/screen.tsx
@@ -1,9 +1,23 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { Canvas } from '@react-three/fiber';
+import { CameraControls } from '@react-three/drei';
 
 const ScreenPage = () => {
-  return null;
+  return (
+    <div className="fixed inset-0">
+      <Canvas>
+        <CameraControls />
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[1, 2, 5]} />
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
+    </div>
+  );
 };
 
 export const Route = createFileRoute('/screen')({
-  component: () => ScreenPage,
+  component: ScreenPage,
 });
