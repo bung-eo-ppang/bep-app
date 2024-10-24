7221d95329d41e4cb9c9c4f9ab41e4de2b6a5fa9
2paperstar <lee@paperst.ar>
2024-10-24 10:24:12 +0900

    feat: open port and receive stream data

--- src/renderer/src/routes/index.tsx
+++ src/renderer/src/routes/index.tsx
@@ -1,7 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router';
 
 import { SerialPort } from 'serialport';
+import z from 'zod';
 import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const rates = [300, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 74880, 115200, 230400, 250000];
 
 const usePorts = () => {
   const [ports, setPorts] = useState<Awaited<ReturnType<typeof SerialPort.list>>>([]);
@@ -13,32 +18,82 @@ const usePorts = () => {
   return ports;
 };
 
+const schema = z.object({
+  path: z.string(),
+  baudRate: z.coerce.number(),
+});
+
 const SelectorPage = () => {
   const ports = usePorts();
   const navigate = Route.useNavigate();
+  const { handleSubmit, register } = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+  });
+  const [port, setPort] = useState<SerialPort>();
 
-  const handleSelect = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    navigate({ to: '/screen' });
+  const handleSelect = (data: z.infer<typeof schema>) => {
+    setPort(new SerialPort(data));
   };
+
+  useEffect(() => {
+    if (!port) return;
+
+    const errorHandler = (error: any) => console.error(error);
+    port.on('error', errorHandler);
+    return () => {
+      port.off('error', errorHandler);
+    };
+  }, [port]);
+
+  useEffect(() => {
+    if (!port) return;
+    if (!port.isOpen) return;
+    console.log('Port is open');
+    const handler = (chunk: any) => console.log(chunk);
+
+    port.on('data', handler);
+
+    return () => {
+      port.off('data', handler);
+      port.close();
+    };
+  }, [port]);
 
   return (
     <div className="grid place-items-center min-h-screen">
-      <div className="flex flex-col gap-2">
-        <h1 className="text-2xl font-bold">Select Board Port</h1>
-        <form className="flex gap-2" onSubmit={handleSelect}>
-          <select name="" id="" className="border border-gray-300 rounded p-1">
+      <form className="flex gap-2 flex-col" onSubmit={handleSubmit(handleSelect)}>
+        <div className="text-2xl font-bold">Select Board Port</div>
+        {ports.length === 0 ? (
+          <div>Loading...</div>
+        ) : (
+          <select
+            {...register('path')}
+            className="border border-gray-300 rounded p-1"
+            defaultValue={ports.find((port) => port.path.includes('usb'))?.path}
+          >
             {ports.map((port) => (
               <option key={port.path} value={port.path}>
                 {port.path}
               </option>
             ))}
           </select>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Connect
-          </button>
-        </form>
-      </div>
+        )}
+        <div className="text-2xl font-bold">Select Baud Rate</div>
+        <select
+          {...register('baudRate')}
+          className="border border-gray-300 rounded p-1"
+          defaultValue={115200}
+        >
+          {rates.map((rate) => (
+            <option key={rate} value={rate}>
+              {rate}
+            </option>
+          ))}
+        </select>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Connect
+        </button>
+      </form>
     </div>
   );
 };
