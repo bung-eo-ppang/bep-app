f3246739071c84430b5fe5aeca11b8119009233c
2paperstar <lee@paperst.ar>
2024-10-28 21:34:47 +0900

    feat: show keypad

--- src/renderer/src/routes/index.tsx
+++ src/renderer/src/routes/index.tsx
@@ -3,8 +3,16 @@ import { createFileRoute } from '@tanstack/react-router';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { EMPTY, mergeAll, mergeMap, of, Subject } from 'rxjs';
 import { SerialPort } from 'serialport';
 import z from 'zod';
+
+const pad = [
+  ['1', '2', '3', 'A'],
+  ['4', '5', '6', 'B'],
+  ['7', '8', '9', 'C'],
+  ['*', 'O', '#', 'D'],
+];
 
 const rates = [300, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 74880, 115200, 230400, 250000];
 
@@ -26,6 +34,9 @@ const schema = z.object({
 const usePort = (option?: ConstructorParameters<typeof SerialPort>[0]) => {
   const [port, setPort] = useState<SerialPort>();
   const [isOpened, setIsOpened] = useState(false);
+  const [subject] = useState(new Subject<Uint8Array>());
+  const [data, setData] = useState<string>();
+  const [connecting, setConnecting] = useState(false);
 
   useEffect(() => {
     if (!option) {
@@ -34,6 +45,7 @@ const usePort = (option?: ConstructorParameters<typeof SerialPort>[0]) => {
     }
     const port = new SerialPort(option);
     setPort(port);
+    setConnecting(true);
 
     return () => {
       port.close();
@@ -43,7 +55,7 @@ const usePort = (option?: ConstructorParameters<typeof SerialPort>[0]) => {
   useEffect(() => {
     if (!port) return;
     const handler = (data: Uint8Array) => {
-      console.log(new TextDecoder().decode(data));
+      subject.next(data);
     };
     port.on('data', handler);
     return () => {
@@ -52,8 +64,14 @@ const usePort = (option?: ConstructorParameters<typeof SerialPort>[0]) => {
   }, [port]);
 
   useEffect(() => {
-    if (!port) return;
-    const handler = () => setIsOpened(port.isOpen);
+    if (!port) {
+      setIsOpened(false);
+      return;
+    }
+    const handler = () => {
+      setIsOpened(port.isOpen);
+      setConnecting(false);
+    };
     port.on('open', handler);
     port.on('close', handler);
     return () => {
@@ -62,7 +80,28 @@ const usePort = (option?: ConstructorParameters<typeof SerialPort>[0]) => {
     };
   }, [port]);
 
-  return port;
+  useEffect(() => {
+    let buffer = new Uint8Array();
+    const subscriber = subject
+      .pipe(
+        mergeAll(),
+        mergeMap((next) => {
+          buffer = new Uint8Array([...buffer, next]);
+          if (next === '\n'.charCodeAt(0)) {
+            const result = new TextDecoder().decode(buffer);
+            buffer = new Uint8Array();
+            return of(result);
+          }
+          return EMPTY;
+        }),
+      )
+      .subscribe(setData);
+    return () => {
+      subscriber.unsubscribe();
+    };
+  }, []);
+
+  return { port, data, isOpened, connecting };
 };
 
 const SelectorPage = () => {
@@ -72,7 +111,7 @@ const SelectorPage = () => {
     resolver: zodResolver(schema),
   });
   const [portData, setPortData] = useState<z.infer<typeof schema>>();
-  const port = usePort(portData);
+  const { data, connecting, isOpened } = usePort(portData);
 
   const handleSelect = (data: z.infer<typeof schema>) => {
     setPortData(data);
@@ -80,7 +119,7 @@ const SelectorPage = () => {
 
   useEffect(() => {
     const handler = (e: BeforeUnloadEvent) => {
-      if (!!port?.isOpen) {
+      if (isOpened) {
         e.preventDefault();
       }
     };
@@ -121,9 +160,29 @@ const SelectorPage = () => {
             </option>
           ))}
         </select>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          {port?.isOpen ? 'Disconnect' : 'Connect'}
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          disabled={connecting}
+        >
+          {isOpened ? 'Disconnect' : 'Connect'}
         </button>
+        {isOpened && (
+          <div className="flex flex-col gap-2 items-center">
+            {pad.map((row, i) => (
+              <div key={i} className="flex gap-2">
+                {row.map((key) => (
+                  <button
+                    key={key}
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    style={{ backgroundColor: data?.includes(key) ? 'red' : 'blue' }}
+                  >
+                    {key}
+                  </button>
+                ))}
+              </div>
+            ))}
+          </div>
+        )}
       </form>
     </div>
   );
