591ab32f48cf2b84d5d72af9ee69246f216ed2d3
2paperstar <lee@paperst.ar>
2024-12-10 09:29:01 +0900

    feat: control menu page with buttons

--- src/renderer/src/routes/menu.tsx
+++ src/renderer/src/routes/menu.tsx
@@ -1,5 +1,7 @@
 import { usePort } from '@renderer/hooks/usePort';
+import { cn } from '@renderer/lib/utils';
 import { createFileRoute, Link, Navigate } from '@tanstack/react-router';
+import { useEffect, useState } from 'react';
 
 const links = [
   { to: '/pingpong', label: 'Ping Pong' },
@@ -7,7 +9,23 @@ const links = [
 ] as const;
 
 const Menu = () => {
-  const { disconnect, isOpened } = usePort();
+  const { disconnect, isOpened, data } = usePort();
+  const [focusIndex, setFocusIndex] = useState(-1);
+  const navigate = Route.useNavigate();
+
+  useEffect(() => {
+    if (data?.buttons[0]) {
+      setFocusIndex((prev) => (prev + 1) % links.length);
+    }
+    if (data?.buttons[1]) {
+      setFocusIndex((prev) => (prev - 1 + links.length) % links.length);
+    }
+    if (data?.buttons[2]) {
+      console.log();
+      navigate({ to: links[focusIndex].to });
+    }
+  }, [data?.buttons[0], data?.buttons[1], data?.buttons[2]]);
+
   if (!isOpened) return <Navigate to="/" />;
   return (
     <div className="flex flex-col min-h-screen items-center gap-8">
@@ -20,11 +38,14 @@ const Menu = () => {
         </button>
       </div>
       <div className="grid p-4 gap-4 grid-cols-4 max-w-5xl flex-1 place-content-start">
-        {links.map(({ to, label }) => (
+        {links.map(({ to, label }, index) => (
           <Link
             to={to}
             key={to}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center h-fit"
+            className={cn(
+              'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center h-fit',
+              index === focusIndex && 'bg-blue-700',
+            )}
           >
             {label}
           </Link>
